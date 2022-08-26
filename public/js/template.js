d712498d49af5f1991418a84a98d20410774f3ab
Richard Choi <richardchoi54@gmail.com>
2022-08-25 20:11:26 -0400

    finished add content to servant template page and started working on layout

--- public/js/template.js
+++ public/js/template.js
@@ -1,11 +1,79 @@
-for(let i = 1; i < 6; i++){
-    document.querySelector(`.link${i}`).addEventListener('click',showTab`${i}`)
+
+
+document.querySelector(`.link1`).addEventListener('click',showTab)
+
+
+function showTab(){
+    for(let i = 1; i < 6; i++){
+        document.querySelector(`#tab${i}`).classList.add('hidden')
+    }
+   document.querySelector(`#tab1`).classList.remove('hidden')
 }
 
-function showTab1(){
+
+document.querySelector(`.link2`).addEventListener('click',showTab2)
+
+
+function showTab2(){
     for(let i = 1; i < 6; i++){
-        document.querySelector(`.link${i}`).classList.add('hidden')
+        document.querySelector(`#tab${i}`).classList.add('hidden')
     }
-    this.classList.remove('hidden')
-    
+   document.querySelector(`#tab2`).classList.remove('hidden')
 }
+
+document.querySelector(`.link3`).addEventListener('click',showTab3)
+
+
+function showTab3(){
+    for(let i = 1; i < 6; i++){
+        document.querySelector(`#tab${i}`).classList.add('hidden')
+    }
+   document.querySelector(`#tab3`).classList.remove('hidden')
+}
+
+document.querySelector(`.link4`).addEventListener('click',showTab4)
+
+
+function showTab4(){
+    for(let i = 1; i < 6; i++){
+        document.querySelector(`#tab${i}`).classList.add('hidden')
+    }
+   document.querySelector(`#tab4`).classList.remove('hidden')
+}
+
+
+if(document.querySelector(`.link5`) !== null && document.querySelector(`#tab5`) !== null){
+
+document.querySelector(`.link5`).addEventListener('click',showTab5)
+
+
+function showTab5(){
+    for(let i = 1; i < 6; i++){
+        document.querySelector(`#tab${i}`).classList.add('hidden')
+    }
+   document.querySelector(`#tab5`).classList.remove('hidden')
+}
+
+}
+
+//  <% let ascension = 1 %>
+// <% for(let keys in info.servant.extraAssets.charaGraph.ascension) { %>
+//  <% if(keys !== 1) { %>
+//  <div id = 'tab<%=ascension%>' class = 'hidden'>
+//  <img src = '<%=info.servant.extraAssets.charaGraph.ascension[keys] %>'>
+//  </div>
+//  <button class ='link<%= ascension %>'>Ascension <%=ascension%></button>
+//  <% ascension+=1 %>
+//  <% } %>
+//  <% } %>
+
+//  <% for(let keys in info.servant.extraAssets.charaGraph.costume) { %>
+//      <button class ='link<%= ascension %>'>Ascension <%=ascension%></button>
+
+//    <% if(info.servant.extraAssets.charaGraph.costume[keys] !== null || info.servant.extraAssets.charaGraph.costume[keys] !== undefined){ %>
+//      <div id = 'tab<%=ascension%>' class = 'hidden'>
+//    <img src = '<%=info.servant.extraAssets.charaGraph.costume[keys] %>'>
+//      </div>
+//    <% } %>
+
+//    <% } %>
