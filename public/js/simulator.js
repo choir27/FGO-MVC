fbf79ea1dd1e6be526cb8485ed232c68be0e08d1
Richard Choi <richardchoi54@gmail.com>
2022-10-17 18:37:44 -0400

    started working on servant 3 and finished servant 2 simple prompts

--- public/js/simulator.js
+++ public/js/simulator.js
@@ -24,7 +24,21 @@ data.skill = info.skill[document.querySelector('#skill1').classList[0]]
 
 document.querySelector('#skill1').addEventListener('click',getSkill1)
 
-function getUrl(effect){
+function getUrl(effect,value){
+  console.log(effect)
+  console.log(value)
+  if(effect.toLowerCase().includes('c.') && effect.toLowerCase().includes('star') && effect.toLowerCase().includes('gain')){
+    document.querySelector('#stars').innerText = value
+   }
+
+   if(effect.toLowerCase().includes('buster') && effect.toLowerCase().includes('up')){
+    return 'https://res.cloudinary.com/choir2/image/upload/v1666045351/Status/Buff_Icon_Buster_Up_uvnlvo.png'
+   }
+
+   if(effect.toLowerCase().includes('np') && effect.toLowerCase().includes('gain')){
+    return 'https://res.cloudinary.com/choir2/image/upload/v1666045412/Status/Buff_Icon_NP_Gain_Up_hnikjh.png'
+   }
+   
   if(effect.toLowerCase().includes('mental') && effect.toLowerCase().includes('up')){
    return 'https://res.cloudinary.com/choir2/image/upload/v1665428012/Status/Buff_Icon_Debuff_Resist_Up_qyng6q.png'
   }
@@ -39,12 +53,23 @@ function getUrl(effect){
 
 function getSkill1(){
   let target = data.skill.servant[0].skills.skills1.funcTargetType
-if(target === 'self'){
+  let effect = data.skill.servant[0].skills.skills1.funcPopupText
+  let value = data.skill.servant[0].skills.skills1.svals[9].Value
+if(target === 'self' && (!effect.includes('C.') || !effect.includes('Star'))){
     let status = document.createElement('img')
-    status.setAttribute('src',getUrl(data.skill.servant[0].skills.skills1.funcPopupText))
+    status.setAttribute('src',getUrl(effect,value))
     document.querySelector('#status1').appendChild(status)
     document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills1.name}.`
-
+}else if(target === 'self'){
+  getUrl(effect,value)
+  document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills1.name}.`
+}
+else if(target === 'ptAll'){
+  let status = document.createElement('img')
+  status.setAttribute('src',getUrl(effect,value))
+  document.querySelector('#status1').appendChild(status)
+  document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
+  document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
 }
 }
 
@@ -52,10 +77,25 @@ document.querySelector('#skill2').addEventListener('click',getSkill2)
   
 function getSkill2(){
   let target = data.skill.servant[0].skills.skills2.funcTargetType
-  if(target === 'self'){
+  let effect = data.skill.servant[0].skills.skills2.funcPopupText
+  let value = data.skill.servant[0].skills.skills2.svals[9].Value
+  if(target === 'self' && (!effect.includes('C.') || !effect.includes('Star'))){
     let status = document.createElement('img')
-    status.setAttribute('src',getUrl(data.skill.servant[0].skills.skills2.funcPopupText))
+    status.setAttribute('src',getUrl(effect,value))
     document.querySelector('#status1').appendChild(status)
+    document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
+}
+  else if(target === 'self'){
+    let status = document.createElement('img')
+    status.setAttribute('src',getUrl(effect,value))
+    document.querySelector('#status1').appendChild(status)
+    document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
+  }
+  else if(target === 'ptAll'){
+    let status = document.createElement('img')
+    status.setAttribute('src',getUrl(effect,value))
+    document.querySelector('#status1').appendChild(status)
+    document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
     document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
   }
 }
@@ -65,11 +105,22 @@ document.querySelector('#skill3').addEventListener('click',getSkill3)
 
 function getSkill3(){  
   let target = data.skill.servant[0].skills.skills3.funcTargetType
-  if(target === 'self'){
+  let effect = data.skill.servant[0].skills.skills3.funcPopupText
+  let value = data.skill.servant[0].skills.skills3.svals[9].Value
+  if(target === 'self' && (!effect.includes('C.') || !effect.includes('Star'))){
     let status = document.createElement('img')
-    status.setAttribute('src',getUrl(data.skill.servant[0].skills.skills3.funcPopupText))
+    status.setAttribute('src',getUrl(effect,value))
     document.querySelector('#status1').appendChild(status)
     document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills3.name}.`
+}
+ else if(target === 'self'){
+    status.setAttribute('src',getUrl(effect,value))
+    document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills3.name}.`
+  } else if(target === 'ptAll'){
+    let status = document.createElement('img')
+    status.setAttribute('src',getUrl(effect,value))
+    document.querySelector('#status1').appendChild(status)
+    document.querySelector('#prompt').innerText = `${data.servant.servant[0].name} used ${data.skill.servant[0].skills.skills2.name}.`
   }
 }
 
