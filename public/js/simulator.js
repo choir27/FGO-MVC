dd76fa769a52d0d802db3fe08b15b66fe5ea84a7
Richard Choi <richardchoi54@gmail.com>
2022-10-21 18:01:02 -0400

    starting to test to make sure using servants at once works properly

--- public/js/simulator.js
+++ public/js/simulator.js
@@ -1,7 +1,19 @@
 let data = {
-  ascension : '',
-  servant : '',
-  skill : '',
+  servant1:{
+    ascension : '',
+    servant : '',
+    skill : '',
+  },
+  servant2:{
+    ascension : '',
+    servant : '',
+    skill : '',
+  },
+  servant3:{
+    ascension : '',
+    servant : '',
+    skill : '',
+  }
 }
 
 for(let i = 0;i < 5; i ++){
@@ -16,18 +28,26 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch('https://fgo-app.herokuapp.com/api/servants')
   .then(res=>res.json())
   .then(info=>{
-data.ascension = info.ascension[document.querySelector('#skill1').classList[0]]
-data.servant = info.servant[document.querySelector('#skill1').classList[0]]
-data.skill = info.skill[document.querySelector('#skill1').classList[0]]
+data.servant1.ascension = info.ascension[document.querySelector('#skill1').classList[0]]
+data.servant1.servant = info.servant[document.querySelector('#skill1').classList[0]]
+data.servant1.skill = info.skill[document.querySelector('#skill1').classList[0]]
+
+data.servant2.ascension = info.ascension[document.querySelector('#skill4').classList[0]]
+data.servant2.servant = info.servant[document.querySelector('#skill4').classList[0]]
+data.servant2.skill = info.skill[document.querySelector('#skill4').classList[0]]
+
+data.servant3.ascension = info.ascension[document.querySelector('#skill8').classList[0]]
+data.servant3.servant = info.servant[document.querySelector('#skill8').classList[0]]
+data.servant3.skill = info.skill[document.querySelector('#skill8').classList[0]]
 })
 }, false);
 
 
 document.querySelector('#skill1').addEventListener('click',getSkill1)
+document.querySelector('#skill4').addEventListener('click',getSkill1)
+document.querySelector('#skill7').addEventListener('click',getSkill1)
 
 function getUrl(effect,value){
-  console.log(effect)
-  console.log(value)
   if(effect.toLowerCase().includes('c.') && effect.toLowerCase().includes('star') && effect.toLowerCase().includes('gain')){
     document.querySelector('#stars').innerText = value
    }
@@ -50,6 +70,7 @@ function getUrl(effect,value){
  return 'https://res.cloudinary.com/choir2/image/upload/v1665430434/Status/Buff_Icon_Guts_nxicxo.png'
   }
 }
+
 
 
 function getSkill1(){
@@ -75,7 +96,9 @@ else if(target === 'ptAll'){
 }
 
 document.querySelector('#skill2').addEventListener('click',getSkill2)
-  
+document.querySelector('#skill5').addEventListener('click',getSkill2)
+document.querySelector('#skill8').addEventListener('click',getSkill2)
+
 function getSkill2(){
   let target = data.skill.servant[0].skills.skills2.funcTargetType
   let effect = data.skill.servant[0].skills.skills2.funcPopupText
@@ -103,6 +126,8 @@ function getSkill2(){
 
 
 document.querySelector('#skill3').addEventListener('click',getSkill3)
+document.querySelector('#skill9').addEventListener('click',getSkill3)
+document.querySelector('#skill6').addEventListener('click',getSkill3)
 
 function getSkill3(){  
   let target = data.skill.servant[0].skills.skills3.funcTargetType
@@ -126,6 +151,8 @@ function getSkill3(){
 
 
 document.querySelector('#help1').addEventListener('click', skillHelp1)
+document.querySelector('#help4').addEventListener('click', skillHelp1)
+document.querySelector('#help6').addEventListener('click', skillHelp1)
 
 function skillHelp1(){
   document.querySelector('#detail').innerText = `${data.skill.servant[0].skills.skills1.detail}`
@@ -133,6 +160,8 @@ function skillHelp1(){
 
 
 document.querySelector('#help2').addEventListener('click', skillHelp2)
+document.querySelector('#help5').addEventListener('click', skillHelp2)
+document.querySelector('#help7').addEventListener('click', skillHelp2)
 
 function skillHelp2(){
   document.querySelector('#detail').innerText = `${data.skill.servant[0].skills.skills2.detail}`
@@ -140,6 +169,8 @@ function skillHelp2(){
 
 
 document.querySelector('#help3').addEventListener('click', skillHelp3)
+document.querySelector('#help6').addEventListener('click', skillHelp3)
+document.querySelector('#help8').addEventListener('click', skillHelp3)
 
 function skillHelp3(){
   document.querySelector('#detail').innerText = `${data.skill.servant[0].skills.skills3.detail}`
